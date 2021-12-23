378b5eb34581da87046c9010ecfbaaf8d6c26820
Andres Perez <amperezp14@gmail.com>
2021-12-23 11:11:58 -0500

    Actualización de proyecto. Configuración inicial del pom de maven

---
+++ JavaTest/src/test/java/com/amperezp14/JavaTest/JavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amperezp14.JavaTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
