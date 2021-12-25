948eceec6ab43f3108e0dfb66939b216d562c7d2
Andres Perez <amperezp14@gmail.com>
2021-12-24 20:44:54 -0500

    Se realiza configuración del archivo application.properties para incluir conexión a bd h2. Se incluye archivo data.sql para el cargue de los datos de la tabla prices

--- JavaTest/src/main/java/com/amperezp14/JavaTest/prices/domain/Price.java
+++ JavaTest/src/main/java/com/amperezp14/JavaTest/prices/domain/Price.java
@@ -3,7 +3,8 @@ package com.amperezp14.JavaTest.prices.domain;
 import java.time.LocalDateTime;
 
 public class Price {
-    int brand_id, price_list, produc_id, priority, price;
+    int brand_id, price_list, product_id, priority;
+    double price;
     String curr;
     LocalDateTime start_date, end_date;
 
