80ae0e33684fb29bf2fbb22a5cf8a5012b795427
Andres Perez <amperezp14@gmail.com>
2022-01-24 02:17:52 -0500

    Se actualiza prueba de endpoint

--- JavaTest/src/main/java/com/amperezp14/JavaTest/prices/domain/IPriceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.amperezp14.JavaTest.prices.domain;
-
-import com.amperezp14.JavaTest.prices.domain.Price;
-
-public interface IPriceRepository {
-
-}
--- JavaTest/src/test/java/com/amperezp14/JavaTest/prices/infraestructure/controller/PriceControllerTest.java
+++ JavaTest/src/test/java/com/amperezp14/JavaTest/prices/infraestructure/controller/PriceControllerTest.java
@@ -1,12 +1,9 @@
 package com.amperezp14.JavaTest.prices.infraestructure.controller;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -16,22 +13,25 @@ import java.io.UnsupportedEncodingException;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(PriceController.class)
+
+@AutoConfigureMockMvc
+@SpringBootTest
 class PriceControllerTest {
+
+    @Test
+    public void contextLoads() {
+    }
 
     @Autowired
     private MockMvc mvc;
 
-    @Mock
-    private JdbcTemplate jdbcTemplate;
 
     @Test
     void getPrice() {
         try {
             RequestBuilder request = post("/prices");
             MvcResult result = mvc.perform(request).andReturn();
-            assertEquals("Hello, World", result.getResponse().getContentAsString());
+            assertEquals("", result.getResponse().getContentAsString());
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         } catch (Exception e) {
